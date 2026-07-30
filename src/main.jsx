c31b4b5435c87044092d51dd8f513de8cab361bd
Gabriele Balatti <gabri.balatti@gmail.com>
2026-07-30 15:35:48 +0200

    Fix bug: aggiornamento del service worker migliorato, ottimizzazione della cache

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { registerSW } from 'virtual:pwa-register';
 import App from './App.jsx';
 import './styles/global.css';
+
+// Registra il Service Worker e controlla subito gli aggiornamenti ad ogni apertura/ricaricamento
+registerSW({ 
+  immediate: true 
+});
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
